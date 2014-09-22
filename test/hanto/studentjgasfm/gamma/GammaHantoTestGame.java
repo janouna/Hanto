27b6207fc7c0b3053ed54ff8bdc23299fcb62052
sfmailand <sam.mailand@gmail.com>
2014-09-22 15:34:04 -0400

    Set up new test files
    Added the new game files needed for gamma hanto, which we'll need moving forward

---
+++ test/hanto/studentjgasfm/gamma/GammaHantoTestGame.java
@@ -0,0 +1,36 @@
+package hanto.studentjgasfm.gamma;
+
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoException;
+import hanto.common.HantoGame;
+import hanto.common.HantoPiece;
+import hanto.common.HantoPieceType;
+import hanto.common.HantoPlayerColor;
+import hanto.common.MoveResult;
+
+public class GammaHantoTestGame implements HantoGame {
+
+	public GammaHantoTestGame(HantoPlayerColor c) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public MoveResult makeMove(HantoPieceType pieceType, HantoCoordinate from,
+			HantoCoordinate to) throws HantoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public HantoPiece getPieceAt(HantoCoordinate where) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPrintableBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
