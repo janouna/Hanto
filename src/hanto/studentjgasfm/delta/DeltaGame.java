10816f9cf9b7892674a63d16510e917f117a54f7
James Anouna <jganouna@wpi.edu>
2014-09-30 14:24:51 -0400

    Added DeltaGame file and turn limit logic

---
+++ src/hanto/studentjgasfm/delta/DeltaGame.java
@@ -0,0 +1,24 @@
+package hanto.studentjgasfm.delta;
+
+import hanto.common.HantoCoordinate;
+import hanto.common.HantoException;
+import hanto.common.HantoPieceType;
+import hanto.common.HantoPlayerColor;
+import hanto.common.MoveResult;
+import hanto.studentjgasfm.common.BaseHantoGame;
+
+public class DeltaGame extends BaseHantoGame {
+
+	public DeltaGame(HantoPlayerColor c) {
+		super(c);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public MoveResult makeMove(HantoPieceType pieceType, HantoCoordinate from,
+			HantoCoordinate to) throws HantoException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
