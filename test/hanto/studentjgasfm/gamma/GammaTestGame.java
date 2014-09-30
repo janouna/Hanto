b8ea93f6fce25757a3722314591a82fcad1fd51b
James Anouna <jganouna@wpi.edu>
2014-09-30 12:37:34 -0400

    Pushing new files missed with last commit

---
+++ test/hanto/studentjgasfm/gamma/GammaTestGame.java
@@ -0,0 +1,31 @@
+package hanto.studentjgasfm.gamma;
+
+import hanto.common.HantoPlayerColor;
+import hanto.studentjgasfm.common.HantoTestGame;
+
+public class GammaTestGame extends GammaGame implements HantoTestGame {
+
+	public GammaTestGame(HantoPlayerColor c) {
+		super(c);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void initializeBoard(PieceLocationPair[] initialPieces) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setTurnNumber(int turnNumber) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setPlayerMoving(HantoPlayerColor player) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
